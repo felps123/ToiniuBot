e0a3d613e462c44cea806f11daa63d9a3415fc17
felps123 <78008769+felps123@users.noreply.github.com>
2021-01-28 18:56:21 -0300

    Update virtex.js

--- src/virtex.js
+++ src/virtex.js
@@ -1,21 +1,21 @@
 const virtex = (prefix, pushname) => {
 	return `Hp Regras`
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
-         '𝗥𝗘𝗚𝗥𝗔𝗦'
+         𝗥𝗘𝗚𝗥𝗔𝗦
 
-ㅤㅤㅤ '[ 𝙋𝙧𝙤𝙞𝙗𝙞𝙙𝙤 ]'
+ㅤㅤㅤ [ 𝙋𝙧𝙤𝙞𝙗𝙞𝙙𝙤 ]
 
-'❌ _Ofensas,Discriminação,preconceito;_'
+❌ _Ofensas,Discriminação,preconceito;_
 
-'❌ _Divulgação de hacks;_'
+❌ _Divulgação de hacks;_
 
-'❌ _Pornografia, gore...;_'
+❌ _Pornografia, gore...;_
 
-'❌ _Flood/spam (Links ou figuras);_'
+❌ _Flood/spam (Links ou figuras);_
 
-'*𝘙𝘦𝘨𝘳𝘢𝘴 𝘱𝘰𝘥𝘦𝘮 𝘴𝘰𝘧𝘳𝘦𝘳 𝘢𝘭𝘵𝘦𝘳𝘢𝘤𝘰𝘦𝘴 𝘴𝘦𝘮 𝘢𝘷𝘪𝘴𝘰 𝘱𝘳𝘦𝘷𝘪𝘰🐊'
+*𝘙𝘦𝘨𝘳𝘢𝘴 𝘱𝘰𝘥𝘦𝘮 𝘴𝘰𝘧𝘳𝘦𝘳 𝘢𝘭𝘵𝘦𝘳𝘢𝘤𝘰𝘦𝘴 𝘴𝘦𝘮 𝘢𝘷𝘪𝘴𝘰 𝘱𝘳𝘦𝘷𝘪𝘰🐊
 
-    '🐊Cuidado com o BAN membro comum.🐊'
+    🐊Cuidado com o BAN membro comum.🐊
 }
 
 exports.virtex = virtex
