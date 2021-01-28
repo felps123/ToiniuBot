39a17167a485379bdb19ff157784e2a4c16be2ab
felps123 <78008769+felps123@users.noreply.github.com>
2021-01-28 17:02:05 -0300

    Create regras.js

---
+++ src/regras.js
@@ -0,0 +1,24 @@
+
+
+const regras = (prefix, pushname) => {
+
+	return `regras 
+  
+  ㅤㅤㅤ   𝗥𝗘𝗚𝗥𝗔𝗦 
+
+ㅤㅤㅤ [ 𝙋𝙧𝙤𝙞𝙗𝙞𝙙𝙤 ]
+
+❌ _Ofensas,Discriminação,preconceito;_
+
+❌ _Divulgação de hacks;_
+
+❌ _Pornografia, gore...;_
+
+❌ _Flood/spam (Links ou figuras);_
+
+*𝘙𝘦𝘨𝘳𝘢𝘴 𝘱𝘰𝘥𝘦𝘮 𝘴𝘰𝘧𝘳𝘦𝘳 𝘢𝘭𝘵𝘦𝘳𝘢𝘤𝘰𝘦𝘴 𝘴𝘦𝘮 𝘢𝘷𝘪𝘴𝘰 𝘱𝘳𝘦𝘷𝘪𝘰🐊*
+  
+  *🐊Cuidado com o BAN membro comum.🐊*
+}
+
+exports.regras = regras
