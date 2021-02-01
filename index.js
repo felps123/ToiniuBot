d38dbccb3532d4d05cfcd5bf8b6e30eaa89bc106
felps123 <78008769+felps123@users.noreply.github.com>
2021-01-31 23:01:16 -0300

    Update test1

--- index.js
+++ index.js
@@ -246,7 +246,7 @@ async function starts() {
 			const isCmd = body.startsWith(prefix)
 
 			mess = {
-				wait: '⌛ Calmaer opoha to fazendo ⌛',
+				wait: '⌛ *iae tiu, virou festa agora? vou começar a cobrar* ⌛',
 				success: '✔️ Sucesso ✔️',
                                 levelon: '❬ ✔ ❭ *habilitar Level*',
 				leveloff: ' ❬ X ❭  *desabilitar Level*',
