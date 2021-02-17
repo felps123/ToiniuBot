92bc294a5fc5712064692ca6d5829151f8b7dcf2
felps123 <78008769+felps123@users.noreply.github.com>
2021-02-17 14:57:08 -0300

    Update and rename toinmenu.js to filipemenu.js

---
+++ src/filipemenu.js
@@ -0,0 +1,12 @@
+const filipemenu = (prefix, pushname) => {
+    return `◪ *𝑭𝒊𝒍𝒊𝒑͢𝒆🐊ᴼ̶ᴿ̶ᴵ̶ᴳ̶ᴵ̶ᴺ̶ᴬ̶ᴸ̶ COMANDOS*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.filipemenu = filipemenu
