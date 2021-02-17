16c82b33959a52424dcf23a7b79a562aacb31828
felps123 <78008769+felps123@users.noreply.github.com>
2021-02-17 15:01:55 -0300

    Update menuadmin.js

--- src/menuadmin.js
+++ src/menuadmin.js
@@ -1,22 +1,21 @@
 const menuadmin = (prefix, pushname) => {
- return `SAALVEE aqui e o menu dos admins
- _Apenas para os mais brabos_
+ return `Apenas para os mais Brabos.
  
  ◪ *🐊COMANDO DOS ADMINS🐊*
  │
- ├─ ❏ ${prefix}opengc
- ├─ ❏ ${prefix}closegc
- ├─ ❏ ${prefix}promote
- ├─ ❏ ${prefix}demote
+ ├─ ❏ ${prefix}add
+ ├─ ❏ ${prefix}ban
+ ├─ ❏ ${prefix}abrirgp
+ ├─ ❏ ${prefix}fechargp
+ ├─ ❏ ${prefix}promover
+ ├─ ❏ ${prefix}rebaixar
  ├─ ❏ ${prefix}tagall
  ├─ ❏ ${prefix}tagall2
  ├─ ❏ ${prefix}tagall3
  ├─ ❏ ${prefix}tagall4
  ├─ ❏ ${prefix}tagall5
- ├─ ❏ ${prefix}add
- ├─ ❏ ${prefix}kick
  ├─ ❏ ${prefix}listadmins
- ├─ ❏ ${prefix}linkgroup
+ ├─ ❏ ${prefix}linkgp
  ├─ ❏ ${prefix}leave
  ├─ ❏ ${prefix}welcome
  ├─ ❏ ${prefix}nsfw
